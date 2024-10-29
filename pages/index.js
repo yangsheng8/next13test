5035e659d14f6fe0f4770c8cff4c5b4622ed9768
yangsheng8 <344182792@qq.com>
2024-10-29 18:38:32 +0800

    chore: gif

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react';
+import { createGIF } from 'gifshot';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+   // 定义 progress 状态
+   const [progress, setProgress] = useState(0);
+   const [gifSrc, setGifSrc] = useState(null);
+ 
+   const handleClick = () => {
+     const images = ['1.png', '2.png', '3.png'];
+ 
+     const options = {
+       images: images,
+       gifWidth: 500,
+       gifHeight: 300,
+       numWorkers: 5,
+       frameDuration: 0.2,
+       sampleInterval: 10,
+       progressCallback: (e) => setProgress(parseInt(e * 100)),
+     };
+ 
+     createGIF(options, (obj) => {
+       if (!obj.error) {
+         setGifSrc(obj.image); // 设置 GIF 图片的 src
+         setProgress(0); // 重置进度
+       }
+     });
+   };
+ 
+   const handleDownload = () => {
+     if (gifSrc) {
+       const link = document.createElement('a');
+       link.download = 'sample.gif';
+       link.href = gifSrc;
+       link.click();
+     }
+   };
+
   return (
     <>
       <Head>
@@ -22,6 +58,16 @@ export default function Home() {
           <div>
           </div>
         </div>
+   <div> 
+          <button onClick={handleClick}>Click to create a GIF</button>
+      {progress > 0 && <p>Creating GIF... {progress}%</p>}
+      {gifSrc && (
+        <>
+          <img src={gifSrc} alt="Generated GIF" style={{ width: '500px', height: '300px', margin: '20px 0' }} />
+          <button onClick={handleDownload}>Download GIF</button>
+        </>
+      )}
+          </div>
       </main>
     </>
   )
